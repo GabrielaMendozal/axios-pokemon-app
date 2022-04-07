4873c34bb5d8d6bff8633c3984aaf5f91ee6200d
GabrielaMendozal <mendozalizethg@gmail.com>
2022-04-07 12:03:22 -0500

    axios pokemon  data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import Pokemon from './Componentes/Pokemon/Pokemon';
+import axios from 'axios';
 
 function App() {
+  const [pokemons, setPokemons] = useState( []);
+  const [URL, setURL] = useState( "https://pokeapi.co/api/v2/pokemon/?limit=807")
+/*
+  useEffect(() =>{
+    let configuracion ={
+      method : "GET"
+    };
+    fetch (URL, configuracion)
+      .then( respuesta => {
+        if( respuesta.ok){
+          return respuesta.json();
+        }
+      })
+      .then( listaPokemons =>{
+        console.log(listaPokemons);
+        if( listaPokemons.results){
+          setPokemons( (pokemonPrev) => listaPokemons.results );
+        }  
+      });
+  }, []);*/
+
+  const buscar = (event) => {
+    event.preventDefault();
+    
+    axios.get(URL)
+      .then( respuesta => {
+        console.log(respuesta);
+        setPokemons( (pokemonPrev) => respuesta.data.results)
+      })
+      .catch( err =>{
+        console.log( err);
+      });
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+          <button type="submit"  onClick={buscar}>
+            Fetch Pokemon
+          </button>
+     
+      <div className='pokemons'>
+        {
+          pokemons.map (( pokemons, indice) =>{
+            return ( <Pokemon key={'pokemons_' + indice} pokemons={pokemons}/>)
+          }
+          )
+        }
+      </div>
     </div>
   );
 }
